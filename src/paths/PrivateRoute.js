b34961872379526572bc0100525ceef30a978449
Shaun Worcester <shaunwo@gmail.com>
2022-10-19 20:19:02 -0400

    Revised file to work with Routes v6

---
+++ src/paths/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+import UserContext from '../private/auth/UserContext';
+
+/** "Higher-Order Component" for private routes.
+ *
+ * In routing component, use these instead of <Route ...>. This component
+ * will check if there is a valid current user and only continues to the
+ * route if so. If no user is present, redirects to login form.
+ */
+
+function PrivateRoute() {
+	const { currentUser } = useContext(UserContext);
+
+	console.debug(
+		'PrivateRoute',
+		'exact=',
+		exact,
+		'path=',
+		path,
+		'currentUser=',
+		currentUser
+	);
+
+	return currentUser ? <Outlet /> : <Navigate to="/login" />;
+}
+
+export default PrivateRoute;
